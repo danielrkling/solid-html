d551a50ed91e046361e55c059a9e1daec9584d50
danielrkling <danielrkling@gmail.com>
2025-08-15 14:20:09 -0400

    test with router

---
+++ tests/router.ts
@@ -0,0 +1,61 @@
+import { A, HashRouter, Route, createAsync, type RouteSectionProps } from "@solidjs/router";
+import { render, Suspense } from "solid-js/web";
+import { h, html, XML, once } from "../src";
+import { createSignal } from "solid-js";
+
+const [time, setTime] = createSignal(0)
+
+setInterval(() => setTime(s => s + 1), 1000)
+
+const xml = XML({
+  Routes,
+  Layout,
+  Route,
+  HashRouter,
+  A,
+});
+
+function Routes() {
+  return xml`<A href="home">Home</A>
+  <A href="about">About Us</A>
+`
+}
+
+// function Mustering(){
+//   return html`<button>MUSTERING</button>`
+// }
+
+function Layout(props: RouteSectionProps) {
+  // const user = createAsync(()=>currentUser())
+  // const role = createAsync(()=>currentRole())
+  // const [pending,start] = useTransition()
+  return xml`
+    <div>
+      <nav>
+        <span time=${time} class="mx-2 flex-1 px-2">SPAN </span>
+        <Routes />
+      </nav>
+    </div>
+    <main class="bg-base-100 w-full overflow-auto grow">
+      ${props.children}
+    </main>
+`;
+}
+
+function App() {
+  return xml`<HashRouter root=${() => (Layout)}>
+    <Route path="/" component=${() => (Home)} />
+    <Route path="/home" component=${() => (Home)} />
+    <Route path="/about" component=${() => About} />
+  </HashRouter>`
+}
+
+function Home() {
+  return xml`Home`
+}
+
+function About() {
+  return "About Us"
+}
+
+render(App, document.getElementById("app")!);
