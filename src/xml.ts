fb8e83262206a2bf2975e70050f0ed17aaf2ec10
danielrkling <danielrkling@gmail.com>
2025-08-27 13:25:31 -0400

    adding template to xml

--- src/xml.ts
+++ src/xml.ts
@@ -1,8 +1,9 @@
+import { assign } from "./assign";
 import { xmlNamespaces } from "./defaults";
 import { H } from "./h";
 import { AssignmentRule, ComponentRegistry } from "./types";
 import { doc, isFunction, isString, toArray } from "./util";
-import { INode, SyntaxKind, parse } from "html5parser";
+import { IAttribute, INode, SyntaxKind, parse } from "html5parser";
 
 //Should be unique character that would never be in the template literal
 const markerStart = '⧙⧙';
@@ -11,17 +12,33 @@ const markerEnd = '⧘⧘';
 //Captures index of hole
 const match = new RegExp(`${markerStart}(\\d+)${markerEnd}`, "g")
 
-const cachedAST = new WeakMap<TemplateStringsArray, INode[]>();
+const cache = new WeakMap<TemplateStringsArray, Template>();
+const walker = doc.createTreeWalker(doc, 133);
 
 
-function getAST(strings: TemplateStringsArray) {
-  let ast = cachedAST.get(strings);
-  if (ast === undefined) {
+type Template = {
+  ast: INode[]
+  attributes?: IAttribute[][]
+  element?: HTMLTemplateElement
+}
+
+
+function getAST(strings: TemplateStringsArray): Template {
+  let template = cache.get(strings);
+  if (template === undefined) {
     //join string with markers and index
-    ast = parse(strings.slice(1).reduce((prev, current, index) => prev + markerStart + index + markerEnd + current, strings[0]))
-    cachedAST.set(strings, ast);
+    const ast = parse(strings.slice(1).reduce((prev, current, index) => prev + markerStart + index + markerEnd + current, strings[0]))
+    template = {
+      ast      
+    }
+    if (!ast.some(hasComponents)){
+      template.element = document.createElement("template");
+      template.attributes = [];
+      buildTemplate(ast,template.element.content, template.attributes!);      
+    }
+    cache.set(strings, template);
   }
-  return ast;
+  return template;
 }
 
 const flat = (arr: any) => (arr.length === 1 ? arr[0] : arr);
@@ -40,6 +57,23 @@ function insertValuesAtMarkers(values: any[], value: string = "", convertMultiPa
 export function XML(components: ComponentRegistry = {}, rules: AssignmentRule[] = []) {
   function xml(template: TemplateStringsArray, ...values: any[]) {
     const cached = getAST(template);
+
+    if(cached.element && cached.attributes){
+      const clone = cached.element.content.cloneNode(true)
+      walker.currentNode = clone;
+
+      let elemIndex = 0
+      while (walker.nextNode()) {
+        const node = walker.currentNode;
+        if (node.nodeType===1){
+          const attributes = cached.attributes[elemIndex++]
+          for (const attribute of attributes){
+            assign(xml.h.rules,node as Element,attribute.name.value,attribute.value?.value)
+          }
+        }
+      }
+
+    }
 
     function nodes(node: INode): any {
       if (node.type === SyntaxKind.Tag) {
@@ -67,6 +101,8 @@ export function XML(components: ComponentRegistry = {}, rules: AssignmentRule[] 
       }
     }
 
+
+
     return flat(toArray(cached).map(nodes));
   }
 
@@ -74,3 +110,39 @@ export function XML(components: ComponentRegistry = {}, rules: AssignmentRule[] 
 
   return xml;
 }
+
+
+function hasComponents(node: INode): boolean | undefined {
+  if (node.type===SyntaxKind.Tag){
+    if (/^[A-Z]/.test(node.rawName)) return  true
+    return node.body?.some(hasComponents)
+  }
+}
+
+function buildTemplate(nodes: INode[], parent: Node, attributes: IAttribute[][]) {
+  for (const node of nodes) {
+    if (node.type === SyntaxKind.Tag) {
+      if (node.name.startsWith("!") || node.name.startsWith("?")) {
+
+      } else {
+        const element = doc.createElement(node.name)
+        attributes.push(node.attributes)
+        buildTemplate(node.body ?? [], element, attributes)
+        parent.appendChild(element)
+      }
+    }
+    if (node.type === SyntaxKind.Text) {
+      const parts = node.value.split(match)
+      parts.forEach((part,i)=>{
+        if (i % 2 ===1){
+          const comment = doc.createComment(markerStart+part+markerEnd)
+          parent.appendChild(comment)
+        } else if (part.trim() !== "") {
+          const text = doc.createTextNode(part)
+          parent.appendChild(text)
+        }
+      })
+    }
+  }
+}
+
