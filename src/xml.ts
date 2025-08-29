43c273a2ef86ca21c9e89f04156cd4307f2e339d
danielrkling <danielrkling@gmail.com>
2025-08-29 09:15:14 -0400

    synced walker

--- src/xml.ts
+++ src/xml.ts
@@ -1,10 +1,46 @@
-import { insert, template } from "solid-js/web";
+import { INode, SyntaxKind, parse } from "html5parser";
+import { insert } from "solid-js/web";
 import { assign } from "./assign";
-import { xmlNamespaces } from "./defaults";
 import { H } from "./h";
 import { AssignmentRule, ComponentRegistry } from "./types";
 import { doc, isFunction, isString, toArray } from "./util";
-import { IAttribute, INode, SyntaxKind, parse } from "html5parser";
+
+type TreeNode = TextNode  | ComponentNode | ElementNode | InsertNode
+
+type ValueParts = Array<string | number>
+
+type Property = [name: string, value: ValueParts]
+
+type TextNode = {
+  type: "text"
+  value: string
+}
+
+type InsertNode = {
+  type: "insert"
+  value: number
+}
+
+type ComponentNode = {
+  type: "component"
+  name: string
+  props: Property[]
+  children: TreeNode[]
+  template: HTMLTemplateElement | null
+}
+
+type ElementNode = {
+  type: "element"
+  name: string
+  props: Property[]
+  children: TreeNode[],
+}
+
+type RootNode = {
+  type: "root"
+  children: TreeNode[]
+  template: HTMLTemplateElement | null
+}
 
 //Should be unique character that would never be in the template literal
 const marker = '⧙⧘';
@@ -12,39 +48,29 @@ const marker = '⧙⧘';
 
 //Captures index of hole
 const match = new RegExp(`${marker}(\\d+)${marker}`, "g")
-const pathMatch = new RegExp(`${marker}(\\d+(?:,\\d+)*)${marker}`, "g")
 
-const cache = new WeakMap<TemplateStringsArray, Cached>();
+const cache = new WeakMap<TemplateStringsArray, RootNode>();
 const walker = doc.createTreeWalker(doc, 133);
 
 
-type Cached = RootNode
-
-
-function getCached(strings: TemplateStringsArray): Cached {
-  let cached = cache.get(strings);
-  if (cached === undefined) {
+function getCachedRoot(strings: TemplateStringsArray): RootNode {
+  let root = cache.get(strings);
+  if (root === undefined) {
     //join string with markers and index
     const ast = parse(strings.slice(1).reduce((prev, current, index) => prev + marker + index + marker + current, strings[0]))
-    const children = ast.map(n => parseNode(n))
+    const children = ast.flatMap(n => parseNode(n))
 
     const template = children.some(n => n.type === "element") ? buildTemplate(children) : null
 
-
-
-    cached = {
+    root = {
       type: "root",
       children,
       template
     }
 
-
-    console.log(cached, template)
-
-
-    cache.set(strings, cached);
+    cache.set(strings, root);
   }
-  return cached;
+  return root;
 }
 
 function flat(arr: any[]) {
@@ -57,62 +83,40 @@ function getValue(value: any) {
 }
 
 
-function insertValuesAtMarkers(values: any[], value: string = "", convertMultiPartToString = false) {
-  const parts = value.split(match).map((v, i) => (i % 2 === 1 ? values[Number(v)] : v)).filter(v => !isString(v) || v.trim())
-  return parts.length === 1 ? parts[0] : convertMultiPartToString ? () => parts.map(getValue).join("") : parts
-}
-
-function getNode(node: ComponentNode, id: string) {
-
-  let current = node as ComponentNode | ElementNode
-  id.split(",").forEach(i => {
-    current = current.children[Number(i)] as ElementNode
-  })
-  return current as ElementNode
-}
-
 export function XML(components: ComponentRegistry = {}, rules: AssignmentRule[] = [], clone = false) {
   function xml(strings: TemplateStringsArray, ...values: any[]) {
-    const cached = getCached(strings);
+    const cached = getCachedRoot(strings);
 
-    function renderTemplate(template: HTMLTemplateElement, componentNode: ComponentNode) {
+    function renderTemplate(template: HTMLTemplateElement, componentNode: ComponentNode | RootNode) {
       const clone = template.content.cloneNode(true)
       walker.currentNode = clone;
 
-      // console.log(componentNode.name || componentNode.type,template.innerHTML)
+      walkNodes(componentNode.children)
 
-      let index = 0
-      while (walker.nextNode()) {
-        const node = walker.currentNode;
-        if (node.nodeType === 1) {
-          const _node = getNode(componentNode, (node as Element).id)
-          // console.log(componentNode,node,_node)
-          for (const [name, parts] of _node.props) {
-            const value = substituteValues(parts, values);
-            assign(xml.h.rules, node as Element, name, value.length === 1 ? value[0] : () => value.map(getValue))
+      function walkNodes(nodes: TreeNode[]) {
+        nodes.forEach(node => {
+          const domNode = walker.nextNode()!;
+          if (node.type === "element") {
+            for (const [name, parts] of node.props) {
+              const value = substituteValues(parts, values);
+              assign(xml.h.rules, domNode as Element, name, value.length === 1 ? value[0] : () => value.map(getValue))
+            }
+            walkNodes(node.children)
+          } else if (node.type === "insert" || node.type === "component") {
+            insert(domNode.parentNode!, renderNode(node), domNode)
+            walker.currentNode = domNode
           }
-        } else if (node.nodeType === 8) {
-          const m = pathMatch.exec(node.nodeValue ?? "")
-          console.log(node.nodeValue,m)
-          if (m) {
-            const _node = getNode(componentNode, m[1])
-            insert(node.parentNode!, renderNode(_node), node)
-          }
-
-        }
+        })
       }
-
       return toArray(clone.childNodes)
     }
 
-    function renderNode(node: MyNode): any {
-      // console.log(node.type,node.value, values)
+    function renderNode(node: TreeNode): any {
 
       if (node.type === "text") {
-        return substituteValues(node.parts, values)
-      } else if (node.type === "comment") {
-        const value = substituteValues(node.parts, values);
-        return xml.h("!", { "prop:textContent": value.length === 1 ? value[0] : () => value.map(getValue) })
+        return node.value
+      } else if (node.type === "insert") {
+        return values[node.value]
       }
 
       // gather props
@@ -123,27 +127,22 @@ export function XML(components: ComponentRegistry = {}, rules: AssignmentRule[] 
       }
 
       // children - childNodes overwrites any props.children
-      if (node.children.length) {
+      if (node.children.length) {     
         if (node.type === "component" && node.template) {
-          props.children = ()=>renderTemplate(node.template, node)
+          props.children = ()=>renderTemplate(node.template!, node)
         } else {
           props.children = () => flat(node.children.map(renderNode));
         }
       }
 
-      // console.log(props)
-
       return xml.h(node.name, props);
-
     }
-
 
     if (cached.template) {
       return renderTemplate(cached.template, cached)
     }
 
-
-    return flat(toArray(cached.children).map(renderNode));
+    return flat(cached.children.map(renderNode));
   }
 
   xml.h = H(components, rules);
@@ -151,140 +150,88 @@ export function XML(components: ComponentRegistry = {}, rules: AssignmentRule[] 
   return xml;
 }
 
-type MyNode = TextNode | CommentNode | ComponentNode | ElementNode
 
-type ValueParts = Array<string | number>
-
-type Property = [name: string, value: ValueParts]
-
-type TextNode = {
-  type: "text"
-  value: string
-  parts: ValueParts
-}
-
-type CommentNode = {
-  type: "comment"
-  value: string
-  parts: ValueParts
-}
-
-type ComponentNode = {
-  type: "component"
-  name: string
-  props: Property[]
-  children: MyNode[]
-  template: HTMLTemplateElement | null
-}
-
-type ElementNode = {
-  type: "element"
-  name: string
-  props: Property[]
-  children: MyNode[],
-}
-
-type RootNode = {
-  type: "root"
-  children: MyNode[]
-  template: HTMLTemplateElement | null
-}
 
 
 function substituteValues(parts: ValueParts, values: any[]) {
   return parts.map(v => isString(v) ? v : values[v])
 }
 
-
+//Split by marker and extract index of hole. Remove empty strings 
 function parseValue(value: string = ""): ValueParts {
   return value.split(match).map((v, i) => (i % 2 === 1 ? Number(v) : v)).filter(v => !isString(v) || v.trim())
 }
 
-function parseNode(node: INode): MyNode {
+
+//Parse html5parser result for what we care about
+function parseNode(node: INode): TreeNode | TreeNode[] {
+
+  //Text nodes are either static text or holes to insert in
   if (node.type === SyntaxKind.Text) {
-    return {
-      type: "text",
-      value: node.value,
-      parts: parseValue(node.value)
-    } as TextNode
+    const parts = parseValue(node.value)
+    return parts.map(value => {
+      const type = isString(value) ? "text" : "insert"
+      return {
+        type,
+        value,
+      } as InsertNode | TextNode
+    })
   }
 
-  if (node.name.startsWith("!") || node.name.startsWith("?")) {
-    const value = node.body?.map(n => n.type === SyntaxKind.Text ? n.value : "").join("") ?? ""
-    const parts = parseValue(value)
-
-    return {
-      type: "comment",
-      value,
-      parts
-    } as CommentNode
+  //Comment Nodes - Do not include in the tree
+  if (node.name[0] === "!" || node.name === "") {
+    console.log(node, node.body?.length)
+    return []
   }
 
-  const children = node.body?.map((n) => parseNode(n)) ?? []
-
-
+  const children = node.body?.flatMap((n) => parseNode(n)) ?? []
   const props = node.attributes.map(v => [v.name.value, parseValue(v.value?.value)]) as Property[]
 
 
   if (/^[A-Z]/.test(node.rawName)) {
+    //Criteria for using template is 1 element. May be be a more optimal condition.
     const hasElementChilden = children.some(v => v.type === "element")
     const template = hasElementChilden ? buildTemplate(children) : null
 
-    const component = {
+    return {
       type: "component",
       name: node.rawName,
       props,
       children,
       template
     } as ComponentNode
-
-    return component
   }
 
-  const element = {
+  return {
     type: "element",
     name: node.name,
     props,
     children,
   } as ElementNode
-
-  return element
 }
 
 
-
-
-function buildTemplate(nodes: MyNode[]): HTMLTemplateElement {
+//build template element with same exact shape as tree so they can be walked through in sync
+function buildTemplate(nodes: TreeNode[]): HTMLTemplateElement {
   const template = doc.createElement("template")
-  buildNodes(nodes, template.content, [])
-
-  function buildNodes(nodes: MyNode[], parent: Node, path: number[]) {
-    nodes.forEach((node, i) => {
-      const newPath = [...path, i]
-      if (node.type === "text") {
-        node.parts.forEach((part, i) => {
-          if (isString(part)) {
-            parent.appendChild(doc.createTextNode(part))
-          } else {
-            parent.appendChild(doc.createComment(marker + newPath + marker))
-          }
-        })
-      } else if (node.type === "comment") {
-
-      } else if (node.type === "component") {
-        parent.appendChild(doc.createComment(marker + newPath + marker))
-        // node.template = buildTemplate(node.children)
-      } else if (node.type === "element") {
-        const elem = doc.createElement(node.name)
-        elem.id = newPath.toString()
-        parent.appendChild(elem)
-        //Apply static attributes here?
-        buildNodes(node.children, elem, newPath)
-      }
-    })
-
-  }
-
+  buildNodes(nodes, template.content)
   return template
 }
 
+
+function buildNodes(nodes: TreeNode[], parent: Node,) {
+  nodes.forEach((node, i) => {
+    if (node.type === "text") {
+      parent.appendChild(doc.createTextNode(node.value))
+    } else if (node.type === "insert") {
+      parent.appendChild(doc.createComment(marker + node.type + marker))
+    } else if (node.type === "component") {
+      parent.appendChild(doc.createComment(marker + node.name + marker))
+    } else if (node.type === "element") {
+      const elem = doc.createElement(node.name)
+      parent.appendChild(elem)
+      //Apply static attributes here?
+      buildNodes(node.children, elem)
+    }
+  })
+}
--- tests/main.ts
+++ tests/main.ts
@@ -8,15 +8,16 @@ function Counter(props) {
 
 
   return xml`
+  <!-- 1 --><?ABC><!ABC <!Aa13   sdfom.asdf!.asd.1!>
     <button class="${"A"}" bool:disabled=${false} style=${count} prop:id="${"A"}B${"C"}" @click=${()=>setCount(v=>v+1)} ><span>${props.children}</span> ${count}</button>
-    <!-- ${count} -->
+    
   `;
 }
 
 xml.h.define({Counter})
 
 function App(){
-    return xml`<div><Counter /><Counter >Count:</Counter></div>` 
+    return xml`<div><Counter />Hello<Counter >Count:</Counter></div>` 
 }
 
 render(App,document.getElementById("app")!)
